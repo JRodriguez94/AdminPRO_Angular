1331661df268ecc1464bae3d0aa940e8f416d9d6
Usuario <usuario@localhost.localdomain>
2019-08-27 12:26:26 -0500

    project base comppleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, Router} from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import {LoginComponent} from './login/login.component';
+import {NopagefoundComponent} from './shared/nopagefound/nopagefound.component';
+import {RegisterComponent} from './login/register.component';
+import {PagesRoutes} from './pages/pages.routes';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NopagefoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [PagesRoutes, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// export const APP_ROUTES = RouterModule.forRoot(routes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import { APP_ROUTES } from './app-routing.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+import { PagesModule } from './pages/pages.module';
+import {PagesRoutes} from './pages/pages.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // APP_ROUTES,
+    AppRoutingModule,
+    PagesModule,
+    PagesRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
